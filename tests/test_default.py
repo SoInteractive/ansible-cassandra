155aa5712e7568c4c3830c5b2c671433dc1b7bca
tgrzesik <Gawariparuski1>
2017-08-25 16:44:15 +0200

    metrics need to be tested. currently only single node works - problems with network in cluster.

--- tests/test_default.py
+++ tests/test_default.py
@@ -10,9 +10,6 @@ def test_linux(SystemInfo):
 def test_packages(Package, SystemInfo):
     present = [
         "python-pip",
-        "apt-transport-https",
-        "ca-certificates",
-        "python-support"
     ]
     if present and SystemInfo.distribution == "ubuntu":
         for package in present:
@@ -22,10 +19,8 @@ def test_packages(Package, SystemInfo):
 
 def test_directories(File):
     present = [
-        "/etc/cassandra",
-        "/var/lib/cassandra/data",
-        "/var/lib/cassandra/commitlog",
-        "/var/lib/cassandra/saved_caches"
+        "/var/lib/docker/volumes/cass-root-dir/_data/",
+        "/var/lib/docker/volumes/cass-data-dir/_data/data",
     ]
     if present:
         for directory in present:
@@ -36,26 +31,25 @@ def test_directories(File):
 
 def test_files(File):
     present = [
-        "/etc/cassandra/cassandra.yaml"
+        "/var/lib/docker/volumes/cass-root-dir/_data/cassandra.yaml",
+        "/var/lib/docker/volumes/cass-root-dir/_data/cassandra-env.sh"
     ]
     if present:
         for file in present:
             f = File(file)
-            if f == "/etc/cassandra/cassandra.yaml":
-                assert f.mode == 0o644
             assert f.exists
             assert f.is_file
 
-
-def test_services(Service):
-    present = [
-        "cassandra"
-        ]
-    for service in present:
-        s = Service(service)
-        assert s.is_enabled
-        assert s.is_running
-
-
-def test_socket(Socket):
-    assert Socket('tcp://0.0.0.0:7080').is_listening
+# TODO
+# def test_services(Service):
+#     present = [
+#         "cassandra"
+#         ]
+#     for service in present:
+#         s = Service(service)
+#         assert s.is_enabled
+#         assert s.is_running
+#
+#
+# def test_socket(Socket):
+#     assert Socket('tcp://0.0.0.0:7080').is_listening
